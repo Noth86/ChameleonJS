d00d1593557457fa02dc4ec9adff82a5fedecec2
Luca Dandria <l.dandria@gmail.com>
2015-06-18 22:49:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+#ChameleonJS
+
+ChameleonJS is a minimalist client-side javascript error logger.
+
+## Usage
+
+To use ChamaleonJS, download it and reference the file from your static files folder in the script tag.
+Copy and paste the script tag into every web page you want to track of javascript errors. Paste it immediately after the opening <head> tag.
+
+```html
+<head>
+    <script src="chameleon.min.js"></script>
+    ...other tags...
+</head>
+```
+
+## Configuration
+
+Edit your chameleon.js file to add your server-side url to track the errors.
+Don't edit other script parts.
+
+```javascript
+//---------------------CONFIGURABLE SETTINGS---------------------
+var parameters = { serverUrlLogger : 'www.fake.url.com/fakeUrl' };
+//---------------------------------------------------------------
+```
+
+## Compatibility
+
+IE7+, Firefox, Chrome, Safari, and Opera.
+
+## Contributors welcome
+
+Contributors are welcome, there is a lot of ideas and new features to implement in the ChameleonJS project.
+Feel free to ask any questions or open any issues.
+
+Enjoy it!
---
+++ chameleon.js
@@ -0,0 +1,54 @@
+/**
+ * ChameleonJS - Error Logger
+ * Logging javascript errors and report them
+ *
+ * @version 0.1
+ * @license MIT license
+ * @author https://github.com/Noth86
+ */
+
+//---------------------CONFIGURABLE SETTINGS---------------------
+var parameters = { serverUrlLogger : 'www.fakeurl.com/fakeUrl' };
+//---------------------------------------------------------------
+
+var chameleonjs = (function (parameters) {
+    'use strict';
+    
+    var exports = {},
+        browser = '',
+        protocol = window.location.protocol === 'https:' ? 'https://' : 'http://',
+        urlToCall = protocol + parameters.serverUrlLogger.replace('https://', '').replace('http://', '');
+        
+    
+    function getBrowserType() {
+        var ua = navigator.userAgent,
+            uaRegExp = ua.match(/(opera|chrome|safari|firefox|msie|trident)\/(\S+)/i);
+        
+        if (uaRegExp) {
+            return uaRegExp[0];
+        }
+        
+        return 'UndefinedBrowser';
+    }
+    
+    function reportError(message, file, line, column, error) {
+        var request = new XMLHttpRequest(),
+            browser = getBrowserType(),
+            data =  'browser=' + browser + '&message=' + message + '&file=' + file +
+                    '&line=' + line + '&column=' + column + '&error=' + error;
+        
+        request.open('POST', urlToCall, true);
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+        request.send(data);
+    }
+
+    exports.init = function () {
+        window.onerror = function (message, file, line, column, error) {
+            reportError(message, file, line, column, error);
+            return true;
+        };
+    };
+    
+    return exports;
+}(parameters));
+chameleonjs.init();
